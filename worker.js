f6b77eddcda4a70de19d3a4fd707c484edb5d96e
DonEvans <don9450-register@yahoo.com>
2021-03-04 14:34:15 -0800

    Tenth worker test

--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
 //Worker test
 onmessage = function(event) {
-  let num1 = JSON.parse(event.data);
-  console.log('Worker: Message received from main script' + num1);
+  let num1 = JSON.parse(event.data[0]);
+  let num2 = JSON.stringify(event.data[1]);
+  console.log('Worker: Message received from main script ' + num1 + " : " + num2);
   
   //let num2 = JSON.parse(event.data[1]);
   //let result = num1 + num2;
--- workerTest.js
+++ workerTest.js
@@ -4,7 +4,8 @@ const myWorker = []
 if (window.Worker) {
   for (let i = 0; i<4; i++) {
     myWorker[i] = new Worker("worker.js");
-    myWorker[i].postMessage(JSON.stringify(i));
+    let num2 = i*i;
+    myWorker[i].postMessage(JSON.stringify(i), JSON.stringify(num2));
     myWorker[i].onmessage = function(event) {
       console.log(`Worker says range is ${JSON.parse(event.data)}`);
     }

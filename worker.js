00ed31fd5739111de79433ebebc55639a741ae44
DonEvans <don9450-register@yahoo.com>
2021-03-04 14:16:41 -0800

    Sixth worker test

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
 //Worker test
 onmessage = function(event) {
   console.log('Worker: Message received from main script');
-  let num1 = JSON.parse(event.data[0]);
-  let num2 = JSON.parse(event.data[1]);
-  let result = num1 + num2;
-  postMessage(JSON.stringify(result));
+  let num1 = JSON.parse(event.data);
+  //let num2 = JSON.parse(event.data[1]);
+  //let result = num1 + num2;
+  postMessage(JSON.stringify(num1));
 }

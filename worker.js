d706f390d69bc695481efa274eed6a4d28352f94
DonEvans <don9450-register@yahoo.com>
2021-03-04 15:06:00 -0800

    15th worker test

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 //Worker test
 onmessage = function(event) {
   let range = JSON.parse(event.data);
-  let msg = "Worker: Message received from main script: ${range[0]} and ${range[1]}";
+  let msg = `Worker: Message received from main script: ${range[0]} and ${range[1]}`;
   console.log(msg);
   
   let num2 = range[0];

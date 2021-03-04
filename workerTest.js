adcce25c9498f85c4c7be3a4ad4e868b9b3571a4
DonEvans <don9450-register@yahoo.com>
2021-03-04 12:14:58 -0800

    Second worker test

--- workerTest.js
+++ workerTest.js
@@ -3,6 +3,9 @@
 if (window.Worker) {
   const myWorker = new Worker("worker.js");
   myWorker.postMessage("Hello from main!");
+  myWorker.onmessage = function(event) {
+    console.log("Message received from worker.");
+  }
 }else {
   console.log('Your browser doesn\'t support web workers.');
 }
